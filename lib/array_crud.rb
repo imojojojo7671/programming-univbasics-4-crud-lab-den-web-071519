4ac83d132765436d9abe2e2535548af9680649a1
Drew Peterson <drew.a.peterson@hotmail.com>
2019-07-04 17:48:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  meal_array = ["snack", "dessert", "lunch", "second breakfast"]
 end
 
 def add_element_to_end_of_array(array, element)
